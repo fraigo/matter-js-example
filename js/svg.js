3a4039bedc20ae11455eff774cc38330128b429e
Francisco <franciscoigor@gmail.com>
2022-02-22 14:39:03 -0800

    fix http load

--- js/svg.js
+++ js/svg.js
@@ -2,14 +2,26 @@ var select = function(root, selector) {
     return Array.prototype.slice.call(root.querySelectorAll(selector));
 };
 
+var loadContent = function(url, callback, error) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.onreadystatechange = function (aEvt) {
+        if (req.readyState == 4) {
+            if(req.status == 200){
+                callback(req.responseText)
+            }
+            else if (error){
+                error('Error loading '+url)
+            }
+        }
+    };
+    req.send(null); 
+};
+
 var loadSvg = function(url, callback) {
-    return fetch(url)
-        .then(function(response) { 
-            return response.text(); 
-        })
-        .then(function(raw) { 
-            callback((new window.DOMParser()).parseFromString(raw, 'image/svg+xml')) 
-        });
+    loadContent(url, function(raw){
+        callback((new window.DOMParser()).parseFromString(raw, 'image/svg+xml'))
+    });
 };
 
 
